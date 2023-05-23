ce43a48ede0a2c819fa3ba447095d5e2a586578c
Patrick Brentlinger <pbrentlinger@gmail.com>
2023-05-22 21:38:19 -0400

    quality of life plugins added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,10 +6,23 @@
 vim.wo.relativenumber = true
 vim.o.termguicolors = true
 vim.o.t_Co = 256
+vim.wo.linebreak=true
+vim.wo.breakindent=true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+
+-- Set the variable g:copilot_no_tab_map to true
+vim.g.copilot_no_tab_map = true
+-- Define the mapping for copilot to <C-l>
+vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 return {
 	'phelipetls/vim-hugo',
 	'tpope/vim-unimpaired',
 	'tpope/vim-surround',
 	'tpope/vim-repeat',
+	'github/copilot.vim',
+	'leafOfTree/vim-matchtag',
 }
