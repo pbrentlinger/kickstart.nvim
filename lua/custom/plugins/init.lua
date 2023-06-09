98da23f2c93fb6ce8c09e15311abd6b659211255
Patrick Brentlinger <pbrentlinger@gmail.com>
2023-06-09 11:17:13 -0400

    codeium is pretty solid and free so I'll use it

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,15 +14,16 @@ vim.o.expandtab = true
 
 
 -- Set the variable g:copilot_no_tab_map to true
-vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_no_tab_map = true
 -- Define the mapping for copilot to <C-l>
-vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+-- vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 return {
 	'phelipetls/vim-hugo',
 	'tpope/vim-unimpaired',
 	'tpope/vim-surround',
 	'tpope/vim-repeat',
-	'github/copilot.vim',
+	-- 'github/copilot.vim',
 	'leafOfTree/vim-matchtag',
+	-- 'Exafunction/codeium.vim',
 }
