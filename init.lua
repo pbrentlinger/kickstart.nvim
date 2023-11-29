8d9f4a71c8a0cd6c9879e79ccf64c553aac9d50d
Patrick Brentlinger <pbrentlinger@gmail.com>
2023-11-29 00:22:44 -0500

    setup function to move a tab with keymaps as I don't want to memorize the functions when I can use keymap pneumonics

--- init.lua
+++ init.lua
@@ -518,6 +518,41 @@ function CloseTabsToLeft()
     end
 end
 
+-- Function to move the current tab by n positions
+function MoveTab(n)
+    local current_tab = vim.fn.tabpagenr()
+    local total_tabs = vim.fn.tabpagenr('$')
+
+    -- Calculate the new tab position
+    local new_tab = current_tab + n
+    if new_tab < 1 then
+        new_tab = 1
+    elseif new_tab > total_tabs then
+        new_tab = total_tabs
+    end
+
+    -- Move to the new tab
+    vim.cmd(tostring(new_tab) .. 'tabmove')
+end
+
+function MoveTabWrapper(positive)
+  local count = vim.v.count or 0
+
+  if not positive then
+    if count <= 0 or nil then
+      count = -2 -- default if no count is passed in, -2 so it actually moves 1 to the left 
+    else
+      count = -math.abs(count +1) -- this part handles the negative count and works as expected
+    end
+  else
+    if count == 0 then
+      count = 1
+    end
+  end
+
+  MoveTab(count)
+end
+
 ---------------------------
 --- Tabpage keybindings ---
 ---------------------------
@@ -526,7 +561,9 @@ vim.api.nvim_set_keymap('n', '<leader>tt', ':tabnew<CR>', { noremap = true, sile
 vim.api.nvim_set_keymap('n', '<leader>tcl', ':lua CloseTabsToLeft()<CR>', { noremap = true, silent = true, desc = 'Close Tabs to the Left' })
 -- Map a key combination to invoke the CloseTabsToRight function
 vim.api.nvim_set_keymap('n', '<leader>tcr', ':lua CloseTabsToRight()<CR>', { noremap = true, silent = true, desc = 'Close Tabs to the Right' })
-
-
+-- Map to move tab positon by n where n can be a positive or negative number meaning to the right or left respectively
+vim.api.nvim_set_keymap('n', '<leader>tmr', ':<C-u>lua MoveTabWrapper(true)<CR>', { noremap = true, silent = true, desc = 'Move Tab [n] times right' })
+vim.api.nvim_set_keymap('n', '<leader>tml', ':<C-u>lua MoveTabWrapper(false)<CR>', { noremap = true, silent = true, desc = 'Move Tab [n] times left' })
+vim.api.nvim_set_keymap('n', '<leader>to', ':tabo', { noremap = true, silent = true, desc = 'Close all other tabs' })
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- v m: ts=2 sts=2 sw=2 et
