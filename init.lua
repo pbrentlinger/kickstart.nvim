eaae175f4d1cfdb21d1eae44f2ae53ab4569fb76
Patrick Brentlinger <pbrentlinger@gmail.com>
2023-12-20 11:35:39 -0500

    synced with laptop changes

--- init.lua
+++ init.lua
@@ -54,7 +54,6 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
