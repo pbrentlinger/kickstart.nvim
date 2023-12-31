0f6ab8cb7c8bb04e9c57d82b21ec814a5c8e5b49
Patrick Brentlinger <pbrentlinger@gmail.com>
2023-12-31 17:47:08 -0500

    brought python indent up to date with kickstarter main

--- init.lua
+++ init.lua
@@ -264,7 +264,7 @@ require('nvim-treesitter.configs').setup {
   auto_install = true,
 
   highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
+  indent = { enable = true },
   incremental_selection = {
     enable = true,
     keymaps = {
