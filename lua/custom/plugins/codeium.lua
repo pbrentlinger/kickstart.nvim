be435ab728c40dec0432a1039573d1c703d029db
Patrick Brentlinger <pbrentlinger@gmail.com>
2024-02-28 23:34:06 -0500

    stopped tab from auto completing because in python it was madness

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -4,6 +4,7 @@ else
 return{
 'Exafunction/codeium.vim',
   config = function ()
+    vim.g.codeium_no_map_tab = 1
     -- Change '<C-g>' here to any keycode you like.
     vim.keymap.set('i', '<C-l>', function () return vim.fn['codeium#Accept']() end, { expr = true })
     vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
