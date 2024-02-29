bea64c3dbc5f0045eaa7760272bd62ca316edaa1
Patrick Brentlinger <pbrentlinger@gmail.com>
2024-02-29 00:34:43 -0500

    codeium tweaks for later maybe

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,17 +1,21 @@
 if vim.g.vscode then
     -- VSCode extension will be used instead
 else
-return{
-'Exafunction/codeium.vim',
-  config = function ()
-    vim.g.codeium_no_map_tab = 1
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-l>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-    vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-    vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-    vim.keymap.set('i', "<c-'>", function() return vim.fn['codeium#Clear']() end, { expr = true })
-    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Close']() end, { expr = true })
-    vim.keymap.set('i', '<c-s>', function() return vim.fn['codeium#Open']() end, { expr = true })
-  end
-}
+    -- local codeium_workspace_root_hints = {'.bzr','.git','.hg','.svn','_FOSSIL_','package.json'}
+
+    return {
+        'Exafunction/codeium.vim',
+        config = function ()
+            -- vim.g.codeium_workspace_root_hints = codeium_workspace_root_hints
+            vim.g.codeium_no_map_tab = 1
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<C-l>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+            vim.keymap.set('i', "<c-'>", function() return vim.fn['codeium#Clear']() end, { expr = true })
+            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Close']() end, { expr = true })
+            -- vim.keymap.set('i', '<c-g>', function() return vim.fn['codeium#Open']() end, { expr = true })
+            -- vim.api.nvim_set_keymap('n', '<leader>cc', '<cmd>' .. vim.fn['codeium#Chat']() .. '<CR>', { noremap = true, silent = true })
+        end
+    }
 end
