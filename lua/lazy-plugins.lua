a066f460f3fbc6e24e87673f67c6b0335a1df852
Patrick <pbrentlinger@gmail.com>
2024-11-21 17:00:53 -0500

    changing to onedark theme for its nicer default config and to sync with windsurf/vscode

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -47,7 +47,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+--  require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/todo-comments',
 
