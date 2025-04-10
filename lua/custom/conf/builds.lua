5f555bdf571d13b31f6b05636122e02a09fec3b5
pbrentlinger <pbrentlinger@gmail.com>
2025-04-10 12:04:49 -0400

    better description because I didn't remember I had made a build previously for asciidoc files and the old desc. seemed dubious

--- lua/custom/conf/builds.lua
+++ lua/custom/conf/builds.lua
@@ -150,7 +150,7 @@ end
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'asciidoc',
   callback = function()
-    vim.keymap.set('n', '<leader>bf', build_run_file, { desc = 'Build file as self-contained package' })
+    vim.keymap.set('n', '<leader>bf', build_run_file, { desc = 'Make Asciidoc optimized PDF and view it' })
     vim.keymap.set('n', '<leader>bt', ':AsciiDocPreview<CR>', { desc = 'Preview Asciidoc file' })
   end,
 })
