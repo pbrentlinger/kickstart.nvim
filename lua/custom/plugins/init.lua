ed7bdc1910325c30ba18e8e7745ed0de851b6479
pbrentlinger <pbrentlinger@gmail.com>
2025-11-01 18:28:51 -0400

    spell files and some custom plugin because I like the eyeliner function ality to make edity my prose easier

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -45,8 +45,8 @@ vim.o.termguicolors = true
 -- vim.o.t_Co = 256
 vim.wo.linebreak = true
 vim.wo.breakindent = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 -- Set local foldmethod to 'indent'
